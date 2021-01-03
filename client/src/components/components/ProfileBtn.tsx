20d13ee42740c24f681b28668c0860ba0fd86ff4
Twiggeh <twiggeh99@gmail.com>
2021-01-03 16:24:52 +0100

    Profile Button Is now mobile friendly

--- client/src/components/components/ProfileBtn.tsx
+++ client/src/components/components/ProfileBtn.tsx
@@ -1,21 +1,46 @@
+import { useTheme } from '@emotion/react';
 import React from 'react';
 import { useHistory } from 'react-router';
 import Button from './Button';
 
 interface IProfileBtn {
 	name: string;
+	css?: string;
 }
 
-const ProfileBtn: React.FC<IProfileBtn> = ({ name }) => {
+const ProfileBtn: React.FC<IProfileBtn> = ({ name, css }) => {
 	const history = useHistory();
+	const theme = useTheme();
 	return (
-		<Button
-			onClick={e => {
-				e.preventDefault();
-				history.push('/profile');
-			}}>
-			Profile of {name}
-		</Button>
+		<>
+			<Button
+				css={`
+					${theme.mq.phone} {
+						display: none;
+					}
+					${css}
+				`}
+				onClick={e => {
+					e.preventDefault();
+					history.push('/profile');
+				}}>
+				Profile of {name}
+			</Button>
+			<Button
+				css={`
+					display: none;
+					${theme.mq.phone} {
+						display: block;
+					}
+					${css}
+				`}
+				onClick={e => {
+					e.preventDefault();
+					history.push('/profile');
+				}}>
+				P
+			</Button>
+		</>
 	);
 };
 
