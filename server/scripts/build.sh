985ef7754953a565aa00854b71db6308c5d290b0
Twiggeh <twiggeh99@gmail.com>
2021-02-01 19:11:07 +0100

    better script resolution

--- server/scripts/build.sh
+++ server/scripts/build.sh
@@ -4,22 +4,22 @@ if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 . "$DIR/parseParameters.sh"
 
 # Set all .env parameters
-echo NODE_ENV=production >.env
-echo SERVER_DIR=$PWD >>.env
+echo NODE_ENV=production >"$DIR/../.env"
+echo SERVER_DIR=$PWD >>"$DIR/../.env"
 
 # DOMAINS
-echo DOMAIN="${arguments[DOMAIN]}" >>.env
-echo SUBDOMAIN="${arguments[SUBDOMAIN]}" >>.env
-echo DOMAINEXTENSION="${arguments[DOMAINEXTENSION]}" >>.env
+echo DOMAIN="${arguments[DOMAIN]}" >>"$DIR/../.env"
+echo SUBDOMAIN="${arguments[SUBDOMAIN]}" >>"$DIR/../.env"
+echo DOMAINEXTENSION="${arguments[DOMAINEXTENSION]}" >>"$DIR/../.env"
 
 # PORTS
-echo SECURE_PORT="${arguments[SECUREPORT]}" >>.env
-echo DEV_PORT="${arguments[DEVELOPMENTPORT]}" >>.env
-echo INSECURE_PORT="${arguments[INSECUREPORT]}" >>.env
+echo SECURE_PORT="${arguments[SECUREPORT]}" >>"$DIR/../.env"
+echo DEV_PORT="${arguments[DEVELOPMENTPORT]}" >>"$DIR/../.env"
+echo INSECURE_PORT="${arguments[INSECUREPORT]}" >>"$DIR/../.env"
 # TODO : add config for production to not use maps
 
 # PROTOCOL
-echo BACKEND_PROTOCOL="${arguments[BACKENDPROTOCOL]}" >>.env
+echo BACKEND_PROTOCOL="${arguments[BACKENDPROTOCOL]}" >>"$DIR/../.env"
 
 # DIRECTORIES
 mkdir -p public/uploads
--- server/scripts/debug.sh
+++ server/scripts/debug.sh
@@ -3,23 +3,23 @@ DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 . "$DIR/parseParameters.sh"
 
-# Set all environ variables, then run nodemon
-echo NODE_ENV=development >.env
-echo SERVER_DIR=$PWD >>.env
+# Set all environment variables, then run nodemon
+echo NODE_ENV=development >"$DIR/../.env"
+echo SERVER_DIR=$PWD >>"$DIR/../.env"
 
 # DOMAINS
-echo DOMAIN="${arguments[DOMAIN]}" >>.env
-echo SUBDOMAIN="${arguments[SUBDOMAIN]}" >>.env
-echo DOMAINEXTENSION="${arguments[DOMAINEXTENSION]}" >>.env
+echo DOMAIN="${arguments[DOMAIN]}" >>"$DIR/../.env"
+echo SUBDOMAIN="${arguments[SUBDOMAIN]}" >>"$DIR/../.env"
+echo DOMAINEXTENSION="${arguments[DOMAINEXTENSION]}" >>"$DIR/../.env"
 
 # PORTS
-echo SECURE_PORT="${arguments[SECUREPORT]}" >>.env
-echo DEV_PORT="${arguments[DEVELOPMENTPORT]}" >>.env
-echo INSECURE_PORT="${arguments[INSECUREPORT]}" >>.env
+echo SECURE_PORT="${arguments[SECUREPORT]}" >>"$DIR/../.env"
+echo DEV_PORT="${arguments[DEVELOPMENTPORT]}" >>"$DIR/../.env"
+echo INSECURE_PORT="${arguments[INSECUREPORT]}" >>"$DIR/../.env"
 # TODO : add config for production to not use maps
 
 # PROTOCOL
-echo BACKEND_PROTOCOL="${arguments[BACKENDPROTOCOL]}" >>.env
+echo BACKEND_PROTOCOL="${arguments[BACKENDPROTOCOL]}" >>"$DIR/../.env"
 
 # DIRECTORIES
 mkdir -p public/uploads
