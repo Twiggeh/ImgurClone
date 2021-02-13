4bbb7ffea30bebda06780866912cb074829579c0
Twiggeh <twiggeh99@gmail.com>
2021-02-13 21:55:20 +0100

    setuserType needs Input & it always returns

--- server/gql/SetUserType.ts
+++ server/gql/SetUserType.ts
@@ -2,6 +2,6 @@ import { gql } from 'apollo-server-express';
 
 export const SetUserType = gql`
 	extend type Mutation {
-		setUser(setUserInput: AddUserInput): GetUserResult
+		setUser(setUserInput: AddUserInput!): GetUserResult!
 	}
 `;
