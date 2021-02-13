708a48f4b85ef9ec03e28cc766b709e718315cbe
Twiggeh <twiggeh99@gmail.com>
2021-02-13 21:51:40 +0100

    fixed gql context returning null for currentUser

--- server/helper/authHelper.ts
+++ server/helper/authHelper.ts
@@ -1,12 +1,21 @@
 import { GetUserFn } from '../gql/GetUserResolver.js';
 import type { MyContext } from '../@types/global';
+import { deNull } from '../utils/utils.js';
 
 export const fetchCurrentUser = async (
 	mongoId: string
 ): Promise<MyContext['currentUser']> => {
 	const data = await GetUserFn({ mongoId });
 	if (data.__typename === 'GetUserSuccess')
-		return { userName: data.userName, profilePicture: data.profilePicture, mongoId };
+		return {
+			userName: data.userName,
+			profilePicture: deNull(data.profilePicture),
+			mongoId,
+		};
 
-	return null;
+	return {
+		userName: undefined,
+		mongoId: undefined,
+		profilePicture: undefined,
+	};
 };
