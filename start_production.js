5161272149297a9304a232a9f52faf5a756696ff
Twiggeh <twiggeh99@gmail.com>
2021-02-14 23:27:20 +0100

    added start production scripts

---
+++ start_production.js
@@ -0,0 +1,40 @@
+import { dirname, join } from 'path';
+import { asyncProcess } from './utils/scriptUtils.js';
+
+const __dirname = decodeURI(dirname(new URL(import.meta.url).pathname));
+
+(async () => {
+	const buildsBuffer = [];
+
+	console.log('Building the production server and client');
+
+	const [serverBuildLock, serverBuildProc] = asyncProcess('node build.js', {
+		cwd: join(__dirname, 'server/scripts'),
+		shell: true,
+	});
+
+	const [clientBuildLock, clientBuildProc] = asyncProcess('node build.js', {
+		cwd: join(__dirname, 'client/scripts'),
+		shell: true,
+	});
+
+	buildsBuffer.push(serverBuildLock, clientBuildLock);
+
+	const results = await Promise.allSettled(buildsBuffer);
+
+	serverBuildProc.kill();
+	clientBuildProc.kill();
+
+	results.forEach((build, i) => {
+		if (build.status === 'fulfilled') {
+			console.log(`Build ${i} succeeded ...`);
+			return;
+		}
+
+		console.log(`Build ${i} failed ...`);
+	});
+
+	console.log('Starting the server ...');
+
+	asyncProcess('node app', { cwd: join(__dirname, 'server/dist/src/app'), shell: true });
+})();
